02de7b0a07c49cdf84b637fa88dc932307bdde36
Hulkstance <warrolen@gmail.com>
2023-02-18 11:53:32 +0200

    Remove GitVersion

--- build/Build.cs
+++ build/Build.cs
@@ -4,7 +4,6 @@ using Nuke.Common.CI;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -18,7 +17,6 @@ partial class Build : NukeBuild
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
-    [GitVersion] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
@@ -47,18 +45,13 @@ partial class Build : NukeBuild
 
     Target Install => _ => _
         .OnlyWhenStatic(() => !IsLocalBuild)
-        .Description("Installs `Nuke.GlobalTool` and `GitVersion.Tool`")
+        .Description("Installs `Nuke.GlobalTool`")
         .Executes(() =>
         {
             DotNetToolInstall(s => s
                 .SetPackageName("Nuke.GlobalTool")
                 .EnableGlobal()
                 .SetVersion("6.3.0"));
-
-            DotNetToolInstall(s => s
-                .SetPackageName("GitVersion.Tool")
-                .EnableGlobal()
-                .SetVersion("5.12.0"));
         });
 
     Target Compile => _ => _
