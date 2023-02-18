781c53b703f1c08de5ab848eb854c1600170143a
Hulkstance <warrolen@gmail.com>
2023-02-18 11:22:37 +0200

    Added Git tags

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -1,59 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitHub;
+using Nuke.Common.Utilities.Collections;
+using Nuke.GitHub;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.GitHub.GitHubTasks;
+using static Nuke.Common.IO.PathConstruction;
 
 [GitHubActions("CI",
     GitHubActionsImage.UbuntuLatest,
     FetchDepth = 0,
     AutoGenerate = true,
     OnPushBranches = new[] { "main" },
-    ImportSecrets = new[] { nameof(NuGetApiKey) },
-    InvokedTargets = new[] { nameof(NuGetPush) })]
+    ImportSecrets = new[] { nameof(NuGetApiKey), nameof(PersonalAccessToken) },
+    InvokedTargets = new[] { nameof(NuGetPush), nameof(GitHubPush), nameof(PublishGithubRelease) })]
 partial class Build
 {
-    [Parameter] string NuGetApiUrl = "https://api.nuget.org/v3/index.json";
-    [Parameter] [Secret] readonly string NuGetApiKey;
+    [Parameter("NuGet API Key")] [Secret] readonly string NuGetApiKey;
+    [Parameter("GitHub Personal Access Token")] [Secret] readonly string PersonalAccessToken;
+
+    [GitRepository] readonly GitRepository GitRepository;
 
     Target Pack => _ => _
+        .Description("Packs the project")
         .DependsOn(Compile)
+        .Produces(ArtifactsDirectory / "*.nupkg")
         .Executes(() =>
         {
             DotNetPack(s => s
+                .SetProject(Solution.GetProject("FluentValidation.OptionsValidation"))
                 .SetConfiguration(Configuration)
-                .SetVersion(GitVersion.NuGetVersionV2)
                 .SetOutputDirectory(ArtifactsDirectory)
-                .EnableNoBuild()
-                .CombineWith(GetPackageProjects(), (s, p) => s.SetProject(p)));
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore()
+                .EnableNoBuild());
         });
 
     Target NuGetPush => _ => _
+        .Description("Pushes the package to NuGet")
         .DependsOn(Pack)
-        .OnlyWhenDynamic(() => IsCiBuild() && IsStableOrRelease())
-        .Requires(() => NuGetApiUrl)
+        .OnlyWhenDynamic(() => IsServerBuild)
         .Requires(() => NuGetApiKey)
         .Executes(() =>
         {
             Assert.True(!string.IsNullOrEmpty(NuGetApiKey));
 
-            var packages = ArtifactsDirectory.GlobFiles("*.nupkg");
-
-            DotNetNuGetPush(s => s
-                .SetSource(NuGetApiUrl)
-                .SetApiKey(NuGetApiKey)
-                .EnableSkipDuplicate()
-                .CombineWith(packages, (s, p) => s
-                    .SetTargetPath(p)));
+            GlobFiles(ArtifactsDirectory, "*.nupkg")
+                .ForEach(x =>
+                {
+                    DotNetNuGetPush(s => s
+                        .SetTargetPath(x)
+                        .SetSource("https://api.nuget.org/v3/index.json")
+                        .SetApiKey(NuGetApiKey)
+                        .EnableSkipDuplicate());
+                });
         });
 
-    IEnumerable<AbsolutePath> GetPackageProjects() => SourceDirectory.GlobFiles("**/**.csproj");
+    Target GitHubPush => _ => _
+        .Description("Pushes the package to GitHub")
+        .DependsOn(Pack)
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Requires(() => PersonalAccessToken)
+        .Executes(() =>
+        {
+            Assert.True(!string.IsNullOrEmpty(PersonalAccessToken));
 
-    bool IsCiBuild() => GitHubActions.Instance != null;
+            GlobFiles(ArtifactsDirectory, "*.nupkg")
+                .ForEach(x =>
+                {
+                    DotNetNuGetPush(s => s
+                        .SetTargetPath(x)
+                        .SetSource($"https://nuget.pkg.github.com/{GitRepository.GetGitHubOwner()}/index.json")
+                        .SetApiKey(PersonalAccessToken)
+                        .EnableSkipDuplicate());
+                });
+        });
 
-    bool IsStableOrRelease() => GitVersion.PreReleaseLabel.Equals("rc", StringComparison.OrdinalIgnoreCase) ||
-                                string.IsNullOrEmpty(GitVersion.PreReleaseLabel);
+    Target PublishGithubRelease => _ => _
+        .DependsOn(Pack)
+        .OnlyWhenDynamic(() => GitRepository.IsOnMainOrMasterBranch())
+        .Requires(() => PersonalAccessToken)
+        .Executes<Task>(async () =>
+        {
+            var nugetPackages = ArtifactsDirectory.GlobFiles("*.nupkg")
+                .Select(x => x.ToString())
+                .ToArray();
+
+            Assert.NotEmpty(nugetPackages);
+
+            await PublishRelease(c => c
+                .SetArtifactPaths(nugetPackages)
+                .SetCommitSha(GitVersion.Sha)
+                .SetRepositoryName(GitRepository.GetGitHubName())
+                .SetRepositoryOwner(GitRepository.GetGitHubOwner())
+                .SetTag($"v{GitVersion.Major}.{GitVersion.Minor}.{GitVersion.Patch}")
+                .SetToken(PersonalAccessToken)
+                .DisablePrerelease());
+        });
 }
